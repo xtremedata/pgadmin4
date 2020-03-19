d8cac2d9d06347388b4172a61915b746d1e13440
rga <rgarbacz@gmail.com>
2020-03-19 01:30:46 -0500

    Progress with dbX profiling tables

--- web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/tables/sql/default/profiling.sql
+++ web/pgadmin/browser/server_groups/servers/databases/schemas/tables/templates/tables/sql/default/profiling.sql
@@ -2,6 +2,6 @@ SELECT
     prfrelid AS {{ conn|qtIdent(_('Profiling 1')) }},
     prfresid AS {{ conn|qtIdent(_('Profiling 2')) }},
     prfauxtopn AS {{ conn|qtIdent(_('Profiling 3')) }},
-    prfauxhist AS {{ conn|qtIdent(_('Profiling 3')) }},
+    prfauxhist AS {{ conn|qtIdent(_('Profiling 3')) }}
 FROM
     dbx_profileinfo
